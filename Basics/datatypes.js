ed473fcb8a7f356772e57bc660981bf88b4d9c10
Vinay Sonekar <108608885+VinaySonekar@users.noreply.github.com>
2023-09-28 08:43:33 +0000

    basics

---
+++ Basics/datatypes.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//datatypes
+let name = "Vinay" //String
+let age = 18 //number
+let isLogged = true //boolean
+let state = null //null
+let colour; //undefined
+// Symbol-> Unique
+
+console.log(typeof 18);
+
+
+//object
+console.log(typeof null);
+console.log(typeof undefined);
---
+++ Basics/variables.js
@@ -0,0 +1,10 @@
+const accountId = 13456
+let accountEmail = "vinay@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+//accountId = 2
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
